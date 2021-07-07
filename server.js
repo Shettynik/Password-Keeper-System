535282d805c6f5ececf27d3e81997b6b1ea5b3c9
Shettynik <nikshitashetty2612@gmail.com>
2021-07-07 12:04:56 +0530

    update structure

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/error");
 const cors = require("cors");
+const path = require("path");
 
 connectDB()
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")))
 app.use(cors());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/private", require("./routes/private"));
